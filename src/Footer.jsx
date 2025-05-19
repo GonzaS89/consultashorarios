d67be38cb123717255a5548e5633690aa10de502
Gonza <gnzx89@gmail.com>
2025-05-19 14:43:30 -0300

    first commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
       <footer className="w-full bg-gray-800 text-gray-200 py-4 mt-8 text-center fixed bottom-0">
         <p className="text-sm sm:text-base">
-          Desarrollado por <span className="font-semibold">Singh Gonzalo</span>
+          Desarrollado por <span className="font-semibold">Sinhg Gonzalo</span>
         </p>
       </footer>
     );
